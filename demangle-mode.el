85ffeee0a3faf1b6e17ae1c2655ebfe9df9d6cda
Ben Liblit <liblit@cs.wisc.edu>
2014-09-12 16:25:15 -0500

    Add home page URL for use with Emacs 24.4's enhanced describe-package.

--- demangle-mode.el
+++ demangle-mode.el
@@ -7,6 +7,7 @@
 ;; Version: 1.0
 ;; Package-Requires: ((emacs "24") (cl-lib "0.1"))
 ;; Keywords: c tools
+;; Homepage: https://github.com/liblit/demangle-mode
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
