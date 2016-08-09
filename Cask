a2b3d92dcd56e3c956bbe94205408d54db2a3781
Ben Liblit <liblit@acm.org>
2016-08-09 15:38:58 -0500

    First attempt to run tests using Travis and Cask

---
+++ Cask
@@ -0,0 +1,8 @@
+;; based on <https://github.com/sviridov/undercover.el-combined-usage-example/blob/master/Cask>
+
+(source gnu)
+(source melpa)
+
+(development
+ (depends-on "ert-runner")
+ (depends-on "faceup"))
---
+++ test/faceup-test.el
@@ -0,0 +1,36 @@
+(require 'demangle-mode)
+(require 'ert)
+(require 'faceup)
+
+(defun fontify-and-drain-demangler (beg end &optional verbose)
+  (font-lock-default-fontify-region beg end verbose)
+  (while (not (tq-queue-empty demangler-queue))
+    (accept-process-output (tq-process demangler-queue) 5)))
+
+(ert-delete-all-tests)
+
+(dolist (raw-name (file-expand-wildcards "test/faceup/*.raw"))
+  (dolist (show-as '(demangled mangled))
+    (let* ((root-name (file-name-base raw-name))
+	   (test-name-string (format "demangle-faceup-test-%s-%s" root-name show-as))
+	   (test-name-symbol (intern test-name-string))
+	   (base-name (file-name-sans-extension raw-name))
+	   (faceup-name (format "%s.%s" base-name show-as)))
+      (eval `(ert-deftest ,test-name-symbol ()
+	       ,(format "compare “%s” with “%s”, showing %s symbols" raw-name faceup-name show-as)
+	       (let ((faceup-properties '(face display help-echo))
+		     (faceup-text (with-temp-buffer
+				    (insert-file-contents ,faceup-name)
+				    (buffer-substring-no-properties (point-min) (point-max))))
+		     (font-lock-fontify-region-function #'fontify-and-drain-demangler))
+		 (with-temp-buffer
+		   (insert-file-contents ,raw-name)
+		   (should (faceup-test-font-lock-buffer
+			    (lambda ()
+			      (demangle-mode)
+			      (demangle-show-as (quote ,show-as)))
+			    faceup-text)))))))))
+
+;; Local variables:
+;; flycheck-disabled-checkers: 'emacs-lisp-checkdoc
+;; End:
