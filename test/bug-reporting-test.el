09036fd05b63eccc4619a14f8ffe6f5ca89bc460
Ben Liblit <liblit@acm.org>
2016-08-10 15:21:56 -0500

    Add tests for GitHub and e-mail bug reporting

---
+++ test/bug-reporting-test.el
@@ -0,0 +1,30 @@
+(require 'undercover)
+(undercover)
+
+(require 'demangle-mode)
+
+(defun demangle-test-interactive-bug-report (use-github)
+  (cl-letf* (((symbol-function #'y-or-n-p) (lambda (prompt) use-github)))
+    (call-interactively #'demangle-mode-submit-bug-report)))
+
+(ert-deftest demangle-test-bug-report-github ()
+  "simulate submitting a bug report using GitHub"
+  (let* ((browsed-urls)
+	 (browse-url-browser-function
+	  (lambda (url &rest args)
+	    (push url browsed-urls))))
+    (demangle-test-interactive-bug-report t)
+    (should (equal '("https://github.com/liblit/demangle-mode/issues") browsed-urls))))
+
+(ert-deftest demangle-test-bug-report-reporter ()
+  "simulate submitting a bug report using reporter"
+  (save-window-excursion
+    (cl-letf* (((symbol-function #'read-string) (lambda (&rest ignored) "canary")))
+      (demangle-test-interactive-bug-report nil))
+    (should (eq major-mode 'message-mode))
+    (set-buffer-modified-p nil)
+    (kill-buffer)))
+
+;; Local variables:
+;; flycheck-disabled-checkers: 'emacs-lisp-checkdoc
+;; End:
