bc6fb49a6755b47e9455fc4743afed1c84cefdb1
Ben Liblit <liblit@acm.org>
2016-08-14 04:28:02 -0500

    Help flycheck find supporting files
    This is a bit inconvenient, as flycheck-emacs-lisp-load-path is
    considered untrusted as a file-local variable.  But I see no other way
    to get flycheck's elisp checker to find what it needs.

--- test/.dir-locals.el
+++ test/.dir-locals.el
@@ -1,5 +1,6 @@
 ((emacs-lisp-mode
-  (flycheck-disabled-checkers quote emacs-lisp-checkdoc)))
+  (flycheck-disabled-checkers quote emacs-lisp-checkdoc)
+  (flycheck-emacs-lisp-load-path "." "..")))
 
 ;; Local variables:
 ;; flycheck-disabled-checkers: 'emacs-lisp
