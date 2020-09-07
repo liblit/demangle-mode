d5bba21841ceb4984f5262eb070caf09f7f08f20
Ben Liblit <liblit@acm.org>
2020-09-07 14:20:54 -0400

    Prepare to release a new version 1.3.3

--- demangle-mode.el
+++ demangle-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ben Liblit <liblit@acm.org>
 ;; Created: 12 Feb 2014
-;; Version: 1.3.2
+;; Version: 1.3.3
 ;; Package-Requires: ((cl-lib "0.1") (emacs "24.3"))
 ;; Keywords: c tools
 ;; URL: https://github.com/liblit/demangle-mode
@@ -293,7 +293,7 @@ Visit `https://github.com/liblit/demangle-mode/issues' or use
 ;;  bug reporting
 ;;
 
-(defconst demangle-mode-version "1.3.2"
+(defconst demangle-mode-version "1.3.3"
   "Package version number for use in bug reports.")
 
 (defconst demangle-mode-maintainer-address "Ben Liblit <liblit@acm.org>"
